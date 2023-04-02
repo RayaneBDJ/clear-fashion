886fbaa2c969039ad1681e59e03d82959f96f10f
Rayane Badji <rayanebadji2711@gmail.com>
2023-04-02 14:47:59 +0200

    API deploy V1

---
+++ server/api_deploy/model/product.js
@@ -0,0 +1,12 @@
+class Product{
+    constructor(brand,rating,product,price)
+    {
+        this.brand = brand;
+        this.rating = rating;
+        this.product = product;
+        this.price = price;
+    }
+}
+
+
+module.exports = {Product};
