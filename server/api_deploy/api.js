5d31249d83d24f37452ba1d88e2068e3dd39891e
Rayane Badji <rayanebadji2711@gmail.com>
2023-04-02 15:16:03 +0200

    api deploy v1

--- server/api_deploy/api.js
+++ server/api_deploy/api.js
@@ -3,24 +3,22 @@ require('dotenv').config()
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 const { connectToDatabase } = require('./db/connect');
-const { Product} = require("./model/product")
+const { Product } = require("./model/product")
+
 const router = express.Router();
-
-
-const PORT = 8092;
-
 const app = express();
+const PORT = process.env.PORT || 8092;
 
-app.get("/", (req,res) => {
-  console.log("This is the first get bro")
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
 
-async function connect(){
-  const db = connectToDatabase(process.env.MONGODB_URI, (erreur) => {
-    if(erreur)
-    {
+async function connect() {
+  const db = await connectToDatabase(process.env.MONGODB_URI, (error) => {
+    if (error) {
       console.log("Error when trying to connect to the database");
       process.exit(-1);
     }
@@ -28,40 +26,20 @@ async function connect(){
   return db;
 }
 
-connect();
-
-app.listen(PORT);
-console.log(`📡 Running on port ${PORT}`);
-
-
-const display_product = async (req,res) => {
-  try{    
-
+const display_product = async (req, res) => {
+  try {
     const db = await connect();
-
     let result = await db.collection("products").find({}).toArray();
-
     res.status(200).json(result);
-  }
-  catch(error){
+  } catch (error) {
     console.log(error);
     res.status(500).json(error);
-
   }
-
-  
 }
 
-router.route("/").get(display_product);
+router.get("/", display_product);
+app.use("/api/v1", router);
 
-app.use(express.urlencoded({extended : true}));
-app.use(express.json());
-
-app.use("/api/v1",router)
-
-
-module.exports = app;
-
-
-
-
+app.listen(PORT, () => {
+  console.log(`📡 Running on port ${PORT}`);
+});
